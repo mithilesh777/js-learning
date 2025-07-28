cb03595ecd9cecaf36c1a95a948eb59b6bc9c310
Mithilesh <51106692+mithilesh777@users.noreply.github.com>
2025-07-28 19:32:24 +0000

    variables and dataType example added

--- 1_basics_prg/dataTypes.js
+++ 1_basics_prg/dataTypes.js
@@ -24,3 +24,26 @@ console.log(typeof m);
 console.log(typeof null);
 console.log(typeof [2,3,1,2,0]);
 console.log(Symbol("id"));
+
+// String to Number or vice versa conversion
+let id = "7";
+let password = 12345;
+console.log(typeof Number(id));
+console.log(typeof String(password));
+
+
+//Special Notes
+let firstName = "TestersZone";
+let lastName = null;
+let isEmailPresent = false;
+let mobileNo = 9087609897;
+let rollNo;
+
+let out = Number(firstName);
+console.log(typeof out);
+console.log(out); // String is getting converted in number but value is NaN
+
+let out1 = Number(rollNo);
+console.log(typeof out1);
+console.log(out1); // String is getting converted in number but value is NaN
+
